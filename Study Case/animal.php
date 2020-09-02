9d5cfb3aa67ae2e7d1219456e53c7775a9188ce4
Suryo Mujahid <55552003+suryomujahid@users.noreply.github.com>
2020-09-03 01:39:18 +0700

    Update animal.php

--- Study Case/animal.php
+++ Study Case/animal.php
@@ -9,12 +9,18 @@ class Hewan{
             $jumlahKaki,
             $jenisPemakan;
  
-  //membuat method untuk Hewan
+  //membuat Constructor untuk Hewan
     public function __construct($namaHewan = "",$jenisKelamin = "",$jumlahKaki = 0,$jenisPemakan = ""){
             $this->namaHewan = $namaHewan;
             $this->jenisKelamin = $jenisKelamin;
             $this->jumlahKaki = $jumlahKaki;
             $this->jenisPemakan = $jenisPemakan;
+        }
+    
+  //membuat Destructor untuk Hewan
+    public function __destruct(){
+            //otomatis akan melakukan ini ketika object tersebut dihapus
+            echo "<br> Object $this->namaHewan telah dihapus";
         }
 
     public function getData(){
@@ -22,8 +28,11 @@ class Hewan{
     }
 }
  
-//membuat object dari class Hewan
+//membuat object dari class Hewan menggunakan Constructor
 $rusa = new Hewan("Rusa", "Jantan", 4, "Herbivora");
 echo "Info hewan pertama: ", $rusa->getData();
+
+//menghapus object rusa untuk melihat Destructor berjalan
+unset($rusa);
  
 ?>
